6eaef894cd66f591918863f5f8d8c9605285db9c
Nia M <unikorn.nkm31@gmail.com>
2024-10-06 18:13:24 -0400

    wrote algorithm I will be using to figure out the symmetric difference of two arrays. will code it when I get the chance.

---
+++ practice/diff.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    //basically finding symmetric difference of two arrays in comparison
+    //with each other
+
+    //loop through array num1
+    //check if the current val in num1 is present in num2 (find)
+    //if its not present in num2 AND answer[0], add to answer[0]
+    //otherwise move on and repeat until you have gone through every val
+    //in num1
+    
+    //repeat for num2
+
+    return 0;
+}
