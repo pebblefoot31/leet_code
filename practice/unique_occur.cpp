f9c6c761ff7056dae24429d5f159680496aecef9
Nia M <unikorn.nkm31@gmail.com>
2024-10-06 18:14:25 -0400

    wrote the algorithm to figure out whether each unique value has a unique number of occurrences within an array of ints. will code when I get the chance to.

---
+++ practice/unique_occur.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+
+    //cerate a vector that keeps track of the 
+    //number of occurrences for each unique val in 
+    //our vector of ints
+    //while looping through ints, if any val occurs the same
+    //num of times as any other val
+    //return false
+    //otherwise, if all unique vals in the vector have a unique
+    //number of occurrences, return true.
+
+    
+    return 0;
+}
