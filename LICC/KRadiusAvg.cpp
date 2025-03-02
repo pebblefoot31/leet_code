fe79c3412b5f5e89e9900b2a91aff2eb5cbdadac
Nia M <unikorn.nkm31@gmail.com>
2025-03-01 20:57:43 -0500

    programmed solution to k-radius problem!

--- LICC/KRadiusAvg.cpp
+++ LICC/KRadiusAvg.cpp
@@ -2,47 +2,57 @@
 #include <vector>
 using namespace std;
 
-/* K-Radius solution without using prefix-sum method. */
+/* K-Radius solution with using prefix-sum method. */
 
 int main() {
 
-    //vector<int> nums = {7,4,3,9,1,8,5,2,6};
-    vector<int> nums = {10000};
+    vector<int> nums = {7,4,3,9,1,8,5,2,6};
+    //vector<int> nums = {40527,53696,10730,66491,62141,83909,78635,18560};
+    vector<int64_t> prefixSums(nums.size());
     vector<int> avgs(nums.size(), -1);
-    int k = 1;
+
+    //int k = 2;
+    int k = 3;
     int minLength = k*2 + 1;
-    int currSum = 0;
-    int currAvg = 0;
+    long long currSum = 0;
+    long long currAvg = 0;
 
+    //returning an array of -1 values if the k-radius is too large for array
     if (nums.size() < minLength) {
-
         cout << "return -1 array." << endl;
 
     } else {
-        
-        for (int i = k; i < (nums.size() - k); i++) {
 
-            for (int j = (i-k); j <= (i + k); j++) {
-               
-                currSum += nums[j];
-                cout << nums[j] << " ";
+        //computing prefix sums array 
+        for (int i = 0; i < nums.size(); i++) {
+
+            currSum += nums[i];
+            prefixSums[i] = currSum;
+        }
+
+        //iterating through radius centerpoints
+        for (int j = k; j < (nums.size() - k); j++) {
+
+            //referring to prefix sums array to determine sum
+            if (j == k) {
+                currSum = prefixSums[j+k];
+            } else {
+                currSum = prefixSums[j+k] - prefixSums[j-k-1];
             }
 
-            cout << endl;
-
+            //computing average + storing in array
             currAvg = currSum / (k*2 + 1);
-            cout << "i: " << i << endl;
-            cout << " currSum : currAvg " << currSum << " : " << currAvg << endl;
-            avgs[i] = currAvg;
-            currSum = 0;
+            avgs[j] = currAvg; 
         }
     }
 
-    for (int l = 0; l < avgs.size(); l++) {
-        cout << avgs[l] << " ";
+    for (int x = 0; x < avgs.size(); x++) {
+        cout << avgs[x] << " ";
     }
 
     cout << endl;
+
+    //expected: [-1,-1,46717,55393,60381,61947,-1,-1]
     
     return 0;
 }
