3fe9a4919b7373d414aae1ffaef9b5d3390d0f0e
Nia M <unikorn.nkm31@gmail.com>
2025-05-20 16:38:53 -0400

    hash: Unfinished attempt at determining how to keep track of the longest subarray with equal 1's and 0's

--- LICC/Hashing/maxContiguous.cpp
+++ LICC/Hashing/maxContiguous.cpp
@@ -3,48 +3,81 @@
 #include <map>
 using namespace std;
 
-// could not solve, come back to this!
-
-
 int main() {
 
     //vector<int> nums = {0,1,1,1,1,1,0,0,0};
     vector<int> nums = {0,1,1,0,0,0,1,0};
     map<int,int> curr;
 
-    int i = 0;
-    int j = i + 1;
+    int left = 0;
+    int right = 1;
+    int currVal = 0;
     int max = 0;
 
-    if (nums.size() != 0) {
+    // if a number is found that causes an unbalance, we move left over and remove elements until frequencies are equal again
+    // otherwise we move right and increase frequency and keep a count of max frequency
+    // but we want it to be CONTIGUOUS?
 
-        while (i < nums.size()-1) {
+    //inserting placeholders, both have a frequency of 0
+    curr.insert(pair<int,int>(0, 0));
+    curr.insert(pair<int,int>(1, 0));
 
-            //resetting
-            curr.clear();
-            for (int z = i; z < j; z++) {
-                curr[nums[z]]++;
+    //inserting the first 2 values
+    curr[nums[left]]++;
+    curr[nums[right]]++;
+
+    while (right < nums.size()) {
+
+        //cout << "window: " << left << " - " << right << endl;
+        //if the number of 0s and 1s is unequal AND if left and right are more than one number away from each other
+        if ((curr[0] != curr[1]) && (right-left > 2)) {
+            cout << "UNEQUAL" << endl;
+            for (int i = left; i <= right; i++) {
+                cout << nums[i];
             }
+            cout << endl;
+
+            //while (left < right) {
+                //decrement count
+             curr[nums[left]]--;
+                //increase left border
+             left++;
+
+             right++;
+             curr[nums[right]]++;
+            //}
+
+        } else {
+            cout << "EQUAL" << endl;
+            for (int j = left; j <= right; j++) {
+                cout << nums[j];
+            }
+            cout << endl;
+
+            right++;
+            curr[nums[right]]++;
+
+            right++;
+            curr[nums[right]]++;
 
             if (curr[0] == curr[1]) {
-                if (curr[0] > max) {
-                    max = curr[0];
+                cout << "EQUAL" << endl;
+                for (int z = left; z <= right; z++) {
+                    cout << nums[z];
                 }
-
-            } else {
-                i++;
-
-                if (j < nums.size() -1) {
-                    j++;
+                cout << endl;
+                currVal = right-left;
+                if (currVal > max) {
+                    max = currVal;
                 }
             }
-
-            
         }
-        
-        cout << max*2 << endl;
+
+
+        //cout << "updated window: " << left << " - " << right << endl;
     }
 
+    cout << currVal << endl;
 
 
 }
