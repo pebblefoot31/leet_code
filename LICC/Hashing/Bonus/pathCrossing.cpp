41868c5cd6a47884dcf2463ee6b2dfafc03aed2c
Nia M <unikorn.nkm31@gmail.com>
2025-05-21 16:03:20 -0400

    hashing (bonus): Determining whether we visited the same point twice

--- LICC/Hashing/Bonus/pathCrossing.cpp
+++ LICC/Hashing/Bonus/pathCrossing.cpp
@@ -6,15 +6,46 @@ using namespace std;
 
 int main() {
 
-    string path = "NESWW";
-    map<int,int> points;
-    currH = 0;
-    currV = 0;
+    string path = "WSSSEWNSNN";
+    map<int,set<int>> points;
+    bool flag = false;
+    int prevH = 0;
+    int prevV = 0;
+    int currH = 0;
+    int currV = 0;
 
+    points[currH].insert(currV);
     for (int i = 0; i < path.size(); i++) {
-        //create a map of coordinate pairs to see pairs visited
-            //track all points and check if there are dupes at the end!
+
+        if (path[i] == 'N') {
+            currV = ++prevV;
+        } else if (path[i] == 'S') {
+            currV = --prevV;
+        } else if (path[i] == 'E') {
+            currH = ++prevH;
+        } else if (path[i] == 'W') {
+            currH = --prevH;
+        }
+        
+        //checking if that combination is in the map of points
+        if ((points.find(currH) != points.end()) && (points[currH].find(currV) != points[currH].end())) {
+            flag = true;
+            break;
+        } 
+
+        points[currH].insert(currV);
+
+        //update for next iteration
+        prevH = currH;
+        prevV = currV;
     }
+
+    if (flag) {
+        cout << "TRUE";
+    } else {
+        cout << "FALSE";
+    }
+    cout << endl;
 
     //if dupes are present, then return false
     //otherwise true
