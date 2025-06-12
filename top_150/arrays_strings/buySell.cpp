d6c5f70d55addfce18136765a29989ef1b90f432
Nia M <unikorn.nkm31@gmail.com>
2025-06-12 14:44:55 -0400

    feat: shorter solution to buySell.cpp

--- top_150/arrays_strings/buySell.cpp
+++ top_150/arrays_strings/buySell.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 int main() {
     
-    vector<int> nums = {7,1,5,3,6,4};
+    vector<int> prices = {7,1,5,3,6,4};
     int profit = 0;
+    int buy = 0;
+    int next_smallest = 0;
+    bool found = false;
 
-    for (int i =0; i < nums.size()-1; i++) {
-        for (int j = i+1; j < nums.size(); j++) {
+    //1. start at index 0 val and do a full run through of the diffeerences
+    //2. then, find the next smallest number and start the next round there (if you 
+    // can't find any smaller number, quit!
 
-            if ((nums[j]-nums[i]) > profit) {
-                profit = nums[j]-nums[i];
+    while (true) {
+
+       for (int i = buy+1; i < prices.size(); i++) {
+           
+            if ((prices[i]-prices[buy]) > profit) {
+                profit = prices[i]-prices[buy];
             }
-        }
+
+            if (!found) {
+                if (prices[i] < prices[buy]) {
+                    next_smallest = i;
+                    found = true;
+                }
+            }
+       }
+
+       if (found) {
+           buy = next_smallest;      
+       } else { 
+           break;
+       }
+
+       found = false;
     }
 
     if (profit <= 0) {
         profit = 0;
     } 
+
     cout << profit << endl;
     return 0;
 }
