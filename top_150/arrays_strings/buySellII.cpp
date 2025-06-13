48a297bfc42f36c45d5b478ff9d0be11733c8bd6
Nia M <unikorn.nkm31@gmail.com>
2025-06-12 22:13:24 -0400

    chore: pseudocode basic algorithm

---
+++ top_150/arrays_strings/buySellII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+
+    //make an array of differencs
+    //if the number is a positive number, add it to the sum
+    //the sum at the end is the max profit
+    return 0;
+}
+
