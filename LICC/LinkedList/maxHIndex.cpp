58d231d5e2e054e3112fcee05a0a7bfe9bceec55
Nia M <unikorn.nkm31@gmail.com>
2025-06-14 23:16:04 -0400

    feat: pseudocode algo for max h index

---
+++ LICC/LinkedList/maxHIndex.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main () {
+
+
+    //sort the array
+    //loop through the array and 
+    //determine whether the nums[i] <= size-i
+    //that becomes the max H until we 
+    //encounter another h that is bigger
+    
+    //3,0,6,1,5
+
+    //0,1,3,5,6
+
+    //0 does not pass, 1 does not pass, 3 passes
+
+    //1,1,3
+
+    //1 does not pass, 1
+
+    
+    return 0;
+}
