4592090b61a2f4e92b488f4dfe86d57a9f7e122b
Nia M <unikorn.nkm31@gmail.com>
2025-06-18 22:20:50 -0400

    failing 12 test cases

--- top_150/arrays_strings/maxHIndex.cpp
+++ top_150/arrays_strings/maxHIndex.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <algorithm>
 using namespace std;
 
@@ -15,50 +16,40 @@ int main () {
     //vector<int> citations = {3,0,6,1,5};
     //vector<int> citations = {1,3,1};
     //vector<int> citations = {0,0,5};
-    vector<int> citations = {11,15};
+    //vector<int> citations = {11,13};
+    vector<int> citations = {1,7,9,4};
+    //vector<int> citations = {100};
+    //vector<int> citations = {0,1,1};
+    //vector<int> citations = {0,0};
     sort(citations.begin(), citations.end());
+    map<int,int> counts;
+    int max = 0;
 
-    int maxCitations  = 0;
-    int numCitations = 0;
-    int maxH = 0;
-
-    if (citations.size() == 1) {
-        if (citations[0] > 0) {
-            maxH = 1;
-        }
-    } else {
-
-        for (int i = 0; i < citations.size(); i++) {
-
-            maxCitations = max(citations[i], maxCitations);
-
-            if (citations[i] <= (citations.size()-i)) {
-                maxH = max(citations[i], maxH);
-            } 
-
-
-            // if (citations[i] > 0 && (i == citations.size()-1)) {
-            //     if (maxH == 0) {
-            //         maxH = 1;
-            //     }
-            // }
+    for (int i = 0; i < citations.size(); i++) {
+        if (citations[i] > 0) {
+            counts[citations[i]]++;
         }
     }
-
-    // if (citations.size() < maxCitations) {
-    //     maxH = min(numCitations, maxCitations); 
-    // }
-
-    cout << maxH << endl;
-
-    //0,1,3,5,6
-
-    //0 does not pass, 1 does not pass, 3 passes
-
-    //1,1,3
-
-    //1 does not pass, 1
-
     
+    map<int,int>::reverse_iterator it = counts.rbegin();
+    int h = 0;
+    int sum = 0;
+    while (it != counts.rend()) {
+
+        sum += it->second;
+        cout << "element " << it->first << endl;
+        cout << "elem count " << it->second << endl;
+        if (it->first <= sum) {
+             h = it->first;
+             break;
+        } else {
+            h = sum;
+        }
+
+        it++;
+    }
+
+    cout << h << endl;
+
     return 0;
 }
