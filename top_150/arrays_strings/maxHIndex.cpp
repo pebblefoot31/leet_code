2aeb9a938e024c3b694304897f064ebc359ba8b8
Nia M <unikorn.nkm31@gmail.com>
2025-06-19 10:58:47 -0400

    solved

--- top_150/arrays_strings/maxHIndex.cpp
+++ top_150/arrays_strings/maxHIndex.cpp
@@ -13,17 +13,10 @@ int main () {
     //that becomes the max H until we 
     //encounter another h that is bigger
     
-    //vector<int> citations = {3,0,6,1,5};
-    //vector<int> citations = {1,3,1};
-    //vector<int> citations = {0,0,5};
-    //vector<int> citations = {11,13};
-    vector<int> citations = {1,7,9,4};
-    //vector<int> citations = {100};
-    //vector<int> citations = {0,1,1};
-    //vector<int> citations = {0,0};
-    sort(citations.begin(), citations.end());
+    vector<int> citations = {3,0,6,1,5};
     map<int,int> counts;
-    int max = 0;
+    int h = 0;
+    int sum = 0;
 
     for (int i = 0; i < citations.size(); i++) {
         if (citations[i] > 0) {
@@ -32,24 +25,28 @@ int main () {
     }
     
     map<int,int>::reverse_iterator it = counts.rbegin();
-    int h = 0;
-    int sum = 0;
-    while (it != counts.rend()) {
 
-        sum += it->second;
-        cout << "element " << it->first << endl;
-        cout << "elem count " << it->second << endl;
-        if (it->first <= sum) {
-             h = it->first;
-             break;
-        } else {
-            h = sum;
+    if (counts.size() == 1) {
+        h = min(it->first, it->second);
+    } else {
+        while (it != counts.rend()) {
+    
+            if (it->first == it->second) {
+                h = max(h,it->second);
+            }
+            
+            sum += it->second;
+            if (sum > it->first) {
+                h = max(h,it->first);
+                break;
+            } else {
+                h = max(h,sum);
+            }
+    
+            it++;
         }
-
-        it++;
     }
 
     cout << h << endl;
-
     return 0;
 }
