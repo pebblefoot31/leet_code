94e50a2278b7d149e7750d5fc649cbd3a49ad0dc
Nia M <unikorn.nkm31@gmail.com>
2025-06-19 16:13:49 -0400

    new section on stacks and queues

---
+++ LICC/Stack_Queue/simplifyPath.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+
+    // iterate through the string in a loop an
+
+    //treat it like a stack and keep track of how
+    //many . you encounter everytime it comes up
+
+    //if i encounter a ..
+    // pop until the next /
+    // if i encounter a .
+    // pop
+    //if i encounter a // or //// or more
+    // pop until only 1 / remaining
+    // if the final character is a /
+    // pop it off
+    
+
+    return 0;
+}
