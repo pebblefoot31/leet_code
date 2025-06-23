8181f55845da163e8a9a9ca7b4852d0e7dc49b0e
Nia M <unikorn.nkm31@gmail.com>
2025-06-22 20:35:54 -0400

    new approach to popping larger segments of path

--- LICC/Stack_Queue/simplifyPath.cpp
+++ LICC/Stack_Queue/simplifyPath.cpp
@@ -1,46 +1,39 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 int main () {
 
     string path = "/a//b////c/d//././/..";
+    istringstream ss(path);
     string ans;
+    string seg;
     int dots = 0;
     int slash = 0;
 
     cout << path << endl;
-    for (int i = 0; i < path.size(); i++) {
 
-        if (!ans.empty() && (ans.back() == '/') && (path[i] == '/')) {
+    while (getline(ss, seg, '/')) {
+        cout << seg << endl;
+
+        if (!ans.empty()) {
+            if (seg == "..") {
+                ans.pop_back();
+            } else if (seg == ".") {
                 continue;
-        }
-
-        if (path[i] == '.') {
-            dots++;
-        } else {
-            if (dots == 1) {
-               continue; 
-            } else if (dots == 2) {
-                while (ans.size() > 1 && ans.back() != '/') {
-                    ans.pop_back();
-                }
-                if (ans.size() > 1 && ans.back() == '/') {
-                    ans.pop_back();
-                }
+            } else {
+                ans.append(seg + "/");
             }
-            dots = 0;
-        }
-
-
-        if (dots == 0 && path[i] != '.') {
-            ans.push_back(path[i]);
+        } else {
+            ans.append(seg + "/");
+            ans.append(seg);
         }
 
     }
 
 
-    //removing / at the end
+    //removing all / at the end
     while (ans.size() > 1 && ans.back() == '/') {
         ans.pop_back();
     }
