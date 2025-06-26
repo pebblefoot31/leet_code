871e2cbc77968b63be652b80645d371f40013283
Nia M <unikorn.nkm31@gmail.com>
2025-06-26 17:14:06 -0400

    feat: solved using the KMP method to find substrings

--- top_150/arrays_strings/needleHay.cpp
+++ top_150/arrays_strings/needleHay.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 int main() {
 
-    string needle = "leeto";
-    string haystack = "leetcode";
+    //string needle = "leeto";
+    string needle = "issip";
+    //string haystack = "leetcode";
+    string haystack = "mississippi";
     int ans = 0;
     int j = 0;
+    int index = 0;
+    bool found = false;
 
+    string::iterator it = haystack.begin();
 
     if (needle.size() > haystack.size()) {
         ans = -1;
     } else {
-        for (int i = 0; i < haystack.size(); i++) {
-            if (haystack[i] == needle[j]) {
-                j++;
-                if (j == needle.size()) {
-                    ans = i - j+1;
+
+        while (((index = haystack.find(needle[0], index)) != string::npos) && !found) {
+            for (int i = index; i < haystack.size(); i++) {
+                if (needle[j] == haystack[i]) {
+                    j++;
+                    if (j == needle.size()) {
+                        ans = index;
+                        found = true;
+                        break;
+                    }
+                } else {
+                    j = 0;
+                    ans = -1;
                     break;
                 }
-            } else {
-                j = 0;
-                ans = -1;
             }
+            index++;
         }
     }
 
-    cout << ans << endl;
+    if (found)
+        cout << ans << endl;
+    else 
+        cout << -1 << endl;
+
     return 0;
 }
