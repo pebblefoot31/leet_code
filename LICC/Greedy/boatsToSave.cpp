12407adb92df18377c5e1e89242cbe2700cd364d
Nia M <unikorn.nkm31@gmail.com>
2025-07-18 14:01:39 -0400

    feat: solving using greedy

--- LICC/Greedy/boatsToSave.cpp
+++ LICC/Greedy/boatsToSave.cpp
@@ -5,41 +5,36 @@ using namespace std;
 
 int main() {
 
-    vector<int> people = {8,2,3,6,2,6};
+    vector<int> people = {8,2,6,2,6};
     sort(people.begin(), people.end());
     int limit = 8;
     int boats = 0;
     bool paired = false;
 
-    int j = people.size()-1;
-
-    while (people.size() != 0) {
+    while (people.size() > 1) {
 
         for (int i = 0; i < people.size(); i++) {
-
-            paired = false;
-            //make a pair successfully
-            if ((people[i] + people[people.size()-1]) <= limit) {
-
-                paired = true;
-                boats += 1;
-                people.pop_back();
-                //remove the pair match
-                remove(people.begin(), people.end(), people[i]);
-                people.pop_back();
-                break;
-            }
+            cout << people[i] << " ";
         }
+        cout << endl;
 
-        if (!paired) {
+        //make a pair successfully
+        if ((people[0] + people[people.size()-1]) <= limit) {
+            //remove the pair match
             boats += 1;
-
-            //remove person that didn't pair
             people.pop_back();
+            people.erase(people.begin());
+
+        } else {
+            people.pop_back();
+            boats += 1;
         }
 
     }
 
+    if (people.size() == 1) {
+        boats+=1;
+    }
     cout << boats << endl;
 
     return 0;
